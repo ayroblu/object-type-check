23f0b6eea999d7f5a093f417ee34f4500ca8fc0b
Ben Lu <ben@ben-l.com>
2017-07-20 19:31:55 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+JS Type Checker
+===============
+
+Specifically this is for JSON to JS type checking, typically from URL calls.
+For better type checking, probably using json-schema is what you want
+
+
---
+++ index.js
@@ -0,0 +1,76 @@
+const schema = {
+    User: {
+        name: {type: 'string'},
+        isActive: {type: 'boolean', isNullable: true, isOptional: true},
+        gps: {type: 'Gps'},
+        thing: {type: 'symbol'},
+        otherNames: {type: 'string', isArray: true},
+    },
+    Gps: {
+        latitude: {type: 'number'},
+        longitude: {type: 'number'},
+    },
+}
+const response = {
+  name: 'Yolo',
+  isActive: null,
+  gps: {latitude: 5, longitude: 4},
+  thing: 'whatever symbols'
+  otherNames: ['first', 'last']
+}
+
+const matcher = new Schema(schema)
+matcher.check('User', response)
+
+class Schema {
+  constructor(schema){
+    if (!schema){
+      throw new Error('Schema not specified')
+    }
+    this.schema = schema
+  }
+  check(type, o){
+    if (typeof o !== 'object') {
+      throw new Error('Object not provided')
+    }
+  }
+}
+
+function checkType(schema, typeName, resp){
+  const schemaType = schema[typeName]
+  if (!schemaType) throw new Error('Type not found')
+  Object.keys(schemaType).forEach(k=>{
+    const {isArray, type} = schemaType[k]
+
+    const oType = /^[A-Z]/.test(type)
+    if (oType){
+      checkType(schema, type, resp[k])
+    }
+    if (isArray) {
+      if (!Array.isArray(resp[k])) {
+        throw new Error(`${k} should be an array`)
+      }
+      resp[k].forEach(r=>{
+        const validType = isValidType(schemaType[k], r)
+
+        if (!validType) {
+          throw new Error(`Invalid type on ${k}, expected ${type}`)
+        }
+      })
+    } else {
+      const validType = isValidType(schemaType[k], resp[k])
+      if (!validType) {
+        throw new Error(`Invalid type on ${k}, expected ${type}`)
+      }
+    }
+  })
+}
+function isValidType(typeDef, o){
+  const {isNullable, isOptional, type} = typeDef
+
+  return [
+    typeof o === type
+  , isNullable && o === null
+  , isOptional && o === undefined
+  ].some(a=>a)
+}
